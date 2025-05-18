9e49b330b36147226f12a56e7edfc57e0b8004ba
chel313 <handarovd@gmail.com>
2025-05-18 10:50:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from pygame import *
 
 window = display.set_mode((700,500))
 background = transform.scale(image.load("3773.jpg"),(700,500))
-ball = transform.scale(image.load("mc.jpg"),(50,50))
-
+font.init()
+font1 = font.Font(None, 72)
+lose1 = font1.render("Player 1 lose",True,(180,0,0))
+lose2 = font1.render("Player 2 lose",True,(180,0,0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed,size_x,size_y):
@@ -19,38 +21,55 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_l(self):
         k_p = key.get_pressed()
-        if k_p[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if k_p[K_DOWN] and self.rect.y < 450:
-            self.rect.y += self.speed
-    
-    def update_r(self):
-        k_p = key.get_pressed()
         if k_p[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         if k_p[K_s] and self.rect.y < 450:
             self.rect.y += self.speed
+    
+    def update_r(self):
+        k_p = key.get_pressed()
+        if k_p[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if k_p[K_DOWN] and self.rect.y < 450:
+            self.rect.y += self.speed
 
-r1 = Player("Rocketka.jpg",450,250,3,100,100)
-r2 = Player("Rocketka.jpg",130,250,3,100,100)
+r1 = Player("Rocketka-Photoroom.png",0,250,3,100,100)
+r2 = Player("Rocketka-Photoroom.png",600,250,3,100,100)
 
-# Rockets = sprite.Group()
-# Rockets.add(r1)
-# Rockets.add(r2)
+ball = GameSprite("mc-Photoroom.png",350,250,2,50,50)
 
+speed_x = 2
+speed_y = 2
 
+finish = False
 game = True 
 while game:
-    window.blit(background,(0,0))
-    window.blit(ball,(350,250))
-    r1.recet()
-    r1.update_l()
-    r2.recet()
-    r2.update_r()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.blit(background,(0,0))
+        ball.recet()
+        r1.recet()
+        r1.update_l()
+        r2.recet()
+        r2.update_r()
 
-    
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if sprite.collide_rect(r1,ball) or sprite.collide_rect(r2,ball):
+            speed_x *= -1
+
+
+        if ball.rect.x < 0:
+            finish = True  
+            window.blit(lose1,(200,230))
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2,(200,230))
 
     display.update()
